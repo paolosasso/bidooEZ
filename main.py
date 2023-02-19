2263d8861a9965a3410c71a94aae9eb5548c97b5
paolosasso <68495386+paolosasso@users.noreply.github.com>
2023-02-19 22:03:20 +0100

    slay

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+import mouse
+print("BIDOO BOT BY STROEA™")
+while True:
+    print("non ancora 😍")
+    if pyautogui.pixel(1357, 313) == (234, 234, 234):
+        mouse.move(1412, 453)
+        #pyautogui.moveTo(1412, 453)
+        mouse.click('left')
+        print(" che fa un gioco di logica")
